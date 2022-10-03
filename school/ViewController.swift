c6888580d98456490a8a7b77ae59f6fab94b9692
keshew <97288599+keshew@users.noreply.github.com>
2022-10-03 16:56:51 +0500

    Initial Commit

---
+++ school/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  school
+//
+//  Created by Артём Коротков on 03.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
